e2b9d91c545ca5708495f1bf8993800bb9ffccd9
Elizabeth Pincheira <elizabeth.pincheira.caro@gmail.com>
2026-01-06 17:12:46 -0300

    se agrega a route home y login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent,LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'findpet_frontend!!!';
-}
+export class AppComponent {}
