070916d8a28a31e60fd97699ab085342760add8b
Elizabeth Pincheira <elizabeth.pincheira.caro@gmail.com>
2026-01-07 19:57:28 -0300

    se crea componente mascotas y se agrega a routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { MainlayoutComponent } from './layout/main-layout/mainlayout.component';
+import { MascotasComponent } from './pages/mascotas/mascotas.component';
 
 export const routes: Routes = [
     { path:'', component:MainlayoutComponent,
         children:[
-            { path: 'home', component: HomeComponent }
+            { path: 'home', component: HomeComponent },
+            { path: 'mascotas', component: MascotasComponent }
         ]}
    ,
    { path: 'login', component: LoginComponent },
